1a38ff5e99aa70278276c27c423a5df143888e3c
Motunrayo Orebiyi <AG2883@Labranet.jamk.fi>
2026-01-13 14:50:23 +0200

    exercises

---
+++ E01/TASK2/environment.js
@@ -0,0 +1,6 @@
+console.log("Node.js version:", process.version);
+console.log("Current working directory:", process.cwd());
+console.log("Operating system platform:", process.platform);
+console.log("Process ID:", process.pid);
+console.log("PATH:", process.env.PATH);
+console.log("USERNAME:", process.env.USERNAME);
